5ea4c03d571476b4939a8388bff465cefe283f9b
HRG <hreyes.gopar@gmail.com>
2020-07-05 18:24:14 -0500

    code to find and copy bed files, create subdirectories, find and copy matrices

--- bin/export-hicproraw.sh
+++ bin/export-hicproraw.sh
@@ -3,21 +3,45 @@
 # variables
 directories_prefix="breast-cancer-MCF10"
 
+# create results directory
+#
+mkdir results
+
 # step 1 is move bed files from data to results
+#
+find -L data/ \
+	-type f \
+	-name "*.bed" \
+	-exec \
+		sh -c 'cp {} results/' \;
+
+# step 2 is figure out the files resolutions
+#
+resolutions=$(ls results/ | cut -d"_" -f2 | sort -u)
+
+# step 3 is creating the subdirectories using
+# the desired prefix
+#
+for i in $resolutions
+	do mkdir results/$directories_prefix-$i
+done  
+
+# step 4 is copying the matrix files that
+# we didn't copy before because their names
+# were a problem to step 2
 #
 find -L data/
 	-type f \
-	-name "*.bed" \
+	-name "*.matrix" \
+	-not -name "*iced*" \
 	-exec \
-		sh -c 'cp {} results' \
+		sh -c 'cp {} results/' \;
 
-# step 2 is create the final directories
-# with the desired prefix
+# step 5 is movig the files to the corresponding
+# directory according to resolution
 #
-find -L results/ \
-	-type f \
-	-name "*.bed" \
-	-exec \
-		sh -c 'cut {} -d"_" -f2 | sort	-u'
+
+
+
 
 
