4d8a6dc3cf11ed199cfa68fdacd001fb2365ea05
HRG <hreyes.gopar@gmail.com>
2020-07-05 21:04:44 -0500

    optimize the copying of files

--- bin/export-hicproraw.sh
+++ bin/export-hicproraw.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # variables
+#
 directories_prefix="breast-cancer-MCF10"
 
 # create results directory
 #
-mkdir results
+mkdir -p results
 
-# step 1 is move bed and matrix files 
-# from data to results
+# step 1 is move bed and matrix files from data to results
 #
 find -L data/ \
-	-type f \
+	\( -type f \
 	-name "*.bed" \
-	-o -name "*.matrix" -not -name "*iced*" \
- 	-exec \
-		sh -c 'cp {} results/' \;
+	-or -name "*.matrix" \
+	-and -not -name "*iced*" \) \
+ 	-exec cp -t results/ {} +
 
 # step 2 is figure out the files resolutions
 #
-resolutions=$(ls results/ | grep bed | cut -d"_" -f2 | sort -u)
+#resolutions=$(ls results/ | grep bed | cut -d"_" -f2 | sort -u)
 
 # step 3 is creating the subdirectories using the desired prefix
 # and moving the bed a and matrix files to them
 #
-for r in $resolutions
-do
-	resolution_dir="results/$directories_prefix-$r"
-	mkdir -p $resolution_dir
+#for r in $resolutions
+#do
+#	resolution_dir="results/$directories_prefix-$r"
+#	mkdir -p "$resolution_dir"
 	
-	mv results/*$r.matrix $resolution_dir
-	#mv results/*$r_abs.bed
-done
+	#mv results/*${r}.matrix "$resolution_dir/"
+	#mv results/*${r}_abs.bed
+#done
 
